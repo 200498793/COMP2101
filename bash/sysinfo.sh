8f4791169f126f9aa670393d835e895365703c20
200498793 <122467940+200498793@users.noreply.github.com>
2023-02-03 14:28:36 -0500

    Create sysinfo.sh

---
+++ bash/sysinfo.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "This is Akash I am here to show u my script below"
+echo "Fully quallified domain name:"
+hostname
+echo "This is the second step which ask us Host Info of my machine:"
+echo "Host Information:"
+hostnamectl
+echo "The Third step ask us about the network information from us :"
+echo "IP address of  this server:"
+echo grep "hellowrld.sh:"
+hostname -I
+echo "The last step in which we discuss about the storage of our root system:"
+echo "The amount of space available in only the root system:"
+df -h /
+echo "Hope u like my script ,though it was fun to do:"
+echo "sincerely"
+echo "Akash Virdi"
