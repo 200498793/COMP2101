98294d2fadee13da21d04dcfdeb0211e80735ffa
200498793 <122467940+200498793@users.noreply.github.com>
2023-02-15 23:51:03 -0500

    Update sysinfo.sh

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 echo "Hello welcome  to my second script"
-#Lion a varible which will give the hostname info
-lion A=$(hostname)
-#lion a variable which will give the fully qualified domain name(FQDN)
-lion B=$(hostname -f)
-#lion C a variable it will provide all the ip address info of the hostname"
-#lion Dvariable and it will give us the system info where awk and tail used
-lion D =$(df -h / awk '{print $3}' tail -n 1)
+#LION1 a varible which will give the hostname info
+Lion1=$(hostname)
+#Lion2 a variable which will give the fully qualified domain name(FQDN)
+Lion2=$(hostname -f)
+#Lion3 a variable it will provide all the ip address info of the hostname"
+Lion3=$(hostname -I)
+#Lion4 a variable and it will give us the system info where awk and tail used
+Lion4=$(df -h / | awk '{print $4}' | tail -n 1)
 cat <<EQF
 "The information of the hostname is"
-Report for:$lion A
+Report for:$Lion1
 ==================================
 "The information of the FQDM is"
-FQDN:$lion B
+FQDN:$Lion2
 ==================================
 "The information of the IP address of the hostname is"
-IP Address:$lion C
+IP Address:$Lion3
 ==================================
 "The information of the available free space on the root filesystem is"
-Root Filesystem  Free space:$lion D
+Root Filesystem  Free space:$Lion4
 ==================================
 EQF
-echo "sincerely"
+
+echo "Sincerely"
 echo "200498793"
